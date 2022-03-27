0351aeac4f4b1c967a8fc6eec7987a396bad5ada
Zoja <zan130208@ya.ru>
2022-03-28 02:09:48 +0300

    add photos

--- src/components/Card.js
+++ src/components/Card.js
@@ -21,7 +21,7 @@ export default function Card({id, name, onAddCatch, isBtnChange}) {
     <div className='card flex'>
       <Link to='/pokecard'>
         <div onClick={onClickShowCard}>
-          {/* <img className="card_img-main" src={srcImg(id)}/>    */}
+          <img className="card_img-main" src={'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/' + (id) + '.png'}/>   
           <div>
             <span className='card_id'>id: {id}</span>
             <p className='card_name'>{name}</p> 
--- src/pages/Caught.jsx
+++ src/pages/Caught.jsx
@@ -7,6 +7,7 @@ export default function Caught({items = []}) {
       <div className="wrapper flex wrap">        
         {items.map((obj) => (
           <div key={obj.id} className='card flex'>
+            <img className="card_img-main" src={'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/' + (obj.id) + '.png'}/>   
             <span className='card_id'>id: {obj.id}</span>
             <p className='card_name'>{obj.name}</p>
           </div>
