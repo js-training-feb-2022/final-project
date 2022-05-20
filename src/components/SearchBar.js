236ee7241d1517138ebc9a419d671e4013d75961
bananazigzags <bananazigzags@gmail.com>
2022-05-20 19:31:55 +0300

    added autofocus on search

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
 
 export default function SearchBar() {
   const [searchPhrase, setSearchPhrase] = React.useState("")
+  const inputRef = useRef(null);
+
+  useEffect(() => inputRef.current.focus(), [])
+
   const handleSubmit = (e) => {
     e.preventDefault();
   }
 
   return (
     <form onSubmit={handleSubmit} class="form-search">
-        <input title="search" value={searchPhrase} onChange={(e) => setSearchPhrase(e.target.value)}></input>
+        <input 
+        title="search" 
+        value={searchPhrase} 
+        onChange={(e) => setSearchPhrase(e.target.value)} 
+        ref={inputRef}
+        ></input>
         <Link to={`/search/${searchPhrase}`} textDecoration="none" className="link">
           <Button variant="contained" type="submit">Search</Button>
         </Link>
